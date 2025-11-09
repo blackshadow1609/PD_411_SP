0e24cf226914a555a25ea013e3f66afc7bc77477
Sky_Time87 <blackshadow1609@yandex.ru>
2025-11-09 17:23:50 +0300

    Car start DONE

--- Car/main.cpp
+++ Car/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+#include <windows.h> //для sleep
 
 using std::cin;
 using std::cout;
@@ -101,6 +102,7 @@ public:
 
 #define MAX_SPEED_LOWER_LIMIT  40
 #define MAX_SPEED_UPPER_LIMIT 400
+
 class Car
 {
 	Engine engine;
@@ -108,6 +110,7 @@ class Car
 	const int MAX_SPEED;
 	int speed;
 	bool driver_inside;
+
 public:
 	Car(double consumption, int volume, int max_speed) :
 		engine(consumption),
@@ -127,18 +130,88 @@ public:
 	{
 		cout << "Your car is over:-(\t" << this << endl;
 	}
+
 	void get_in()
 	{
 		driver_inside = true;
 		panel();
 	}
+
 	void get_out()
 	{
 		driver_inside = false;
+		system("CLS");
+		cout << "You are out of the car" << endl;
 	}
+
+	void start_engine()
+	{
+		if (tank.get_fuel_level() > 0)
+		{
+			engine.start();
+			cout << "Engine started!" << endl;
+		}
+		else
+		{
+			cout << "Can't start engine: no fuel!" << endl;
+		}
+	}
+
+	void stop_engine()
+	{
+		engine.stop();
+		cout << "Engine stopped!" << endl;
+	}
+
+	void drive()
+	{
+		if (!engine.started())
+		{
+			cout << "Start the engine first!" << endl;
+			return;
+		}
+
+		if (tank.get_fuel_level() <= 0)
+		{
+			cout << "No fuel! Can't drive!" << endl;
+			engine.stop();
+			return;
+		}
+
+		cout << "Enter speed (0-" << MAX_SPEED << " km/h): ";
+		int new_speed;
+		cin >> new_speed;
+
+		if (new_speed < 0) new_speed = 0;
+		if (new_speed > MAX_SPEED) new_speed = MAX_SPEED;
+
+		speed = new_speed;
+
+		if (speed > 0)
+		{
+			cout << "Driving at " << speed << " km/h" << endl;
+
+			double consumption_multiplier = 1.0 + (speed / 100.0);
+			double fuel_consumed = engine.get_consumption_per_second() * consumption_multiplier;
+
+			if (fuel_consumed > tank.get_fuel_level())
+			{
+				fuel_consumed = tank.get_fuel_level();
+				cout << "Low fuel! Slowing down..." << endl;
+				speed = 0;
+			}
+
+			cout << "Fuel consumed: " << fuel_consumed << " liters" << endl;
+		}
+		else
+		{
+			cout << "Car stopped" << endl;
+		}
+	}
+
 	void control()
 	{
-		cout << "Press 'Enter' to get in" << endl;
+		cout << "Press 'Enter' to get in/out" << endl;
 		char key;
 		do
 		{
@@ -146,33 +219,110 @@ public:
 			switch (key)
 			{
 			case Enter:
-				if (driver_inside)get_out();
-				else get_in();
+				if (driver_inside)
+				{
+					get_out();
+					
+					cout << "\nControls outside car:" << endl;
+					cout << "I - Info" << endl;
+					cout << "F - Fill tank" << endl;
+					cout << "E - Enter car" << endl;
+					cout << "ESC - Exit program" << endl;
+				}
+				else
+				{
+					get_in();
+				}
 				break;
 			case 'F':
 			case 'f':
-				double amount;
-				cout << "How much do you want? ";
-				cin >> amount;
-				tank.fill(amount);
+				if (!driver_inside)
+				{
+					double amount;
+					cout << "How much fuel do you want? ";
+					cin >> amount;
+					tank.fill(amount);
+					cout << "Tank filled. Current fuel level: " << tank.get_fuel_level() << " liters" << endl;
+				}
+				break;
+			case 'I':
+			case 'i':
+				if (!driver_inside)
+				{
+					info();
+				}
+				break;
+			case 'S':
+			case 's':
+				if (driver_inside)
+				{
+					if (engine.started())
+					{
+						stop_engine();
+					}
+					else
+					{
+						start_engine();
+					}
+				}
+				break;
+			case 'D':
+			case 'd':
+				if (driver_inside)
+				{
+					drive();
+				}
 				break;
 			}
+
+			if (driver_inside && engine.started() && tank.get_fuel_level() > 0)
+			{
+				
+				double idle_consumption = engine.get_consumption_per_second() * 0.3;
+				if (idle_consumption < tank.get_fuel_level())
+				{
+					//здесь должен быть учет времени
+					cout << "Engine running... consuming fuel" << endl;
+				}
+				else
+				{
+					cout << "Out of fuel! Engine stopped!" << endl;
+					engine.stop();
+				}
+			}
+
 		} while (key != Escape);
 	}
+
 	void panel()const
 	{
 		while (driver_inside)
 		{
 			system("CLS");
-			cout << "Fuel level:\t" << tank.get_fuel_level() << " liters.\n";
-			cout << "Engine is " << (engine.started() ? "started" : "stopped") <<  endl;
+			cout << "=== CAR PANEL ===" << endl;
+			cout << "Fuel level:\t" << tank.get_fuel_level() << " liters\n";
+			cout << "Engine is " << (engine.started() ? "STARTED" : "STOPPED") << endl;
+			cout << "Speed:\t" << speed << " km/h\n";
+			cout << "Max speed:\t" << MAX_SPEED << " km/h\n";
+			cout << "\nControls:" << endl;
+			cout << "S - Start/Stop engine" << endl;
+			cout << "D - Drive" << endl;
+			cout << "F - Fill tank" << endl;
+			cout << "Enter - Get out" << endl;
+			cout << "ESC - Exit program" << endl;
+
+			
+			if (_kbhit()) break;
+			Sleep(100);  
 		}
 	}
+
 	void info()const
 	{
 		engine.info();
 		tank.info();
 		cout << "Max speed:\t" << MAX_SPEED << " km/h\n";
+		cout << "Current speed:\t" << speed << " km/h\n";
 	}
 };
 
